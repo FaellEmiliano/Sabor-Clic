c533aa2ce9db7554288963fc32550f440e9c3580
Sávio Pereira Soares <saviopereirasoares6@gmail.com>
2026-09-03 19:25:54 -0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv(r'C:/Users/Sávio/Desktop/simul_integrado/.env')
-s = os.getenv('F_KEY')
-assert s is not None
-SECRET_KEY = s.encode('utf-8')
